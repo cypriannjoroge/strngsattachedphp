9fbb7cf9da25d936bf7581b7f4c9a6dfc3b92a12
njoro <cypriannjoroge4@gmail.com>
2025-07-26 19:56:33 +0300

    admin  controllers and routes creation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\admin\adminmaincontroller;
+use App\Http\Controllers\admin\categorycontroller;
+use App\Http\Controllers\admin\productattributecontroller;
+use App\Http\Controllers\admin\productcontroller;
+use App\Http\Controllers\admin\productdiscountattributecontroller;
+use App\Http\Controllers\admin\subcategorycontroller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,10 +16,71 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified','rolemanager:user'])->name('dashboard');
-Route::get('admin/dashboard', function () {
-    return view('admin');
-})->middleware(['auth', 'verified','rolemanager:admin'])->name('admin');
-Route::get('vendor/dashboard', function () {
+
+//admin routes
+Route::middleware(['auth', 'verified','rolemanager:admin'])->group(function () {
+  Route::prefix('admin')->group(function(){
+      Route::controller(adminmaincontroller::class)->group(function(){
+          Route::get('/dashboard','index')->name('admin');
+          Route::get('/settings','setting')->name('admin.settings');
+          Route::get('/manage/users','manage_user')->name('admin.manage.user');
+          Route::get('/manage/stores','manage_stores')->name('admin.manage.store');
+          Route::get('/cart/history','cart_history')->name('admin.cart.history');
+          Route::get('/order/history','order_history')->name('admin.order.history');
+      });
+
+      Route::controller(categorycontroller::class)->group(function(){
+          Route::get('/category/create','index')->name('category.create');
+          Route::get('/category/manage','manage')->name('category.manage');
+          
+      });
+
+       Route::controller(subcategorycontroller::class)->group(function(){
+          Route::get('/subcategory/create','index')->name('subcategory.create');
+          Route::get('/subcategory/manage','manage')->name('subcategory.manage');
+      
+      });
+
+      Route::controller(productcontroller::class)->group(function(){
+          Route::get('/product/manage','index')->name('product.manage');
+          Route::get('/product/review/manage','review-manage')->name('product.review.manage');
+      
+      });
+      
+      Route::controller(productattributecontroller::class)->group(function(){
+          Route::get('/productattribute/create','index')->name('productattribute.create');
+          Route::get('/productattribute/manage','manage')->name('productattribute.manage');
+      
+      });
+
+      Route::controller(productdiscountattributecontroller::class)->group(function(){
+          Route::get('/discount/create','index')->name('discount.create');
+          Route::get('/discount/manage','manage')->name('discount.manage');
+      
+      });
+  });
+});
+
+
+//vendor routes
+Route::middleware(['auth', 'verified','rolemanager:vendor'])->group(function () {
+  Route::controller(adminmaincontroller::class)->group(function () {
+    Route::prefix('vendor')->group(function () {
+      Route::get('/dashboard','index')->name('vendor');
+      });
+   });
+});
+
+//user routes
+Route::middleware(['auth', 'verified','rolemanager:user'])->group(function () {
+  Route::controller(adminmaincontroller::class)->group(function () {
+    Route::prefix('user')->group(function () {
+      Route::get('/dashboard','index')->name('user');
+      });
+   });
+});
+
+Route::get('/vendor/dashboard', function () {
     return view('vendor');
 })->middleware(['auth', 'verified','rolemanager:vendor'])->name('vendor');
 
